216d828018dc439fcba9a2adc777a06050dbfba4
Antonioo <167131112+Kipper20@users.noreply.github.com>
2024-04-16 17:48:02 +0300

    Delete sortedscript.sh

--- sortedscript.sh
+++
@@ -1,7 +0,0 @@
-
-
-#!/bin/bash
-
-find ~/mybash/sorted/ -name '*.txt' -exec mv -t ~/mybash/sorted/txtfiles/ -- {} +
-
-find ~/mybash/sorted/ -name '*.key' -exec mv -t ~/mybash/sorted/keysfiles/ -- {} +
